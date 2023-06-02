2a51701272a8d45ce46d5f8fc194ab366975b990
Eetche <108818620+Eetche@users.noreply.github.com>
2023-06-02 14:13:25 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,23 +3,57 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const { Server } = require("socket.io")
-const io = new Server(server)
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+const io = new Server(server, {
+    maxHttpBufferSize: 1e8
 })
 
+app.get('/', (req, res) => {
+    res.sendFile( __dirname + '/registration.html')
+})
+
+app.use(express.static(__dirname))
+
+let online = io.sockets.sockets.size
+
 io.on('connection', (socket) => {
+    online = io.sockets.sockets.size
+
     console.log('user connected')
 
-    socket.on('disconnect', (pseudo) => {
-        console.log(pseudo + " disconnected")
+    socket.emit('connec', online)
+
+    socket.join("global")
+
+    socket.on('disconnect', () => {
+        online = io.sockets.sockets.size
+        socket.emit("disconnec", online + 1)
+        socket.leave(socket.rooms[0])
+        console.log("user disconnected")
     });
 
     socket.on('chat message', (data) => {
-        console.log("pseudo: " + data.pseudo)
-        console.log("message: " + data.input)
-        io.emit('chat message', data)
+        console.log(socket.rooms)
+        if (socket.rooms.size == 3) {
+            socket.to("global").emit("chat message", data)
+            console.log("socket don't have room")
+        } else {
+            socket.to(socket.room).emit("chat message", data)
+        }
+    })
+
+    socket.on('inputing', (data) => {
+        socket.broadcast.emit('inputing', data)
+    })
+
+    socket.on('join', (data) => {
+        if (socket.rooms.size < 3) {
+            socket.leave('global')
+            socket.join(data.roomName)
+            console.log(socket.rooms)
+            socket.room = data.roomName
+            console.log("id: " + socket.id + " join to room named: " + data.roomName)
+            socket.emit("join", data)
+        }
     })
 });
 
